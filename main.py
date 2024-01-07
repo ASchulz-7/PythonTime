77580053e8b287335dc4c0ebf8befa3600ce23f5
Alex <148474825+ASchulz-7@users.noreply.github.com>
2024-01-07 14:27:29 -0500

    Update main.py
    Updated  to include final draft of code, code pulls all information on the books and sends to csv file

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-#from typing imp
 import requests
 from bs4 import BeautifulSoup
 import csv
 
 import re
 import os
-import shutil
 from pathlib import Path
 
 
@@ -18,67 +16,60 @@ def category_selection():
         # create a list for all links of the categories:
         all_categories = []
         soup = BeautifulSoup(response.content, "html.parser")
-        categories = soup.select(".side_categories a")
+        categories= soup.select(".side_categories a")
         for category in categories:
             href = category["href"]
             link = f"https://books.toscrape.com/{href}"
             all_categories.append(link)
         return all_categories
-print(category_selection())
-
-
-
-# print(category_selection(book_links))
 
 # extract all book links within each category
 def extract_book_links(category_url):
     print('----------start book category-----------')
-    response = requests.get(category_url)
-    if response.status_code == 200:
-        print('yay')
-    else:
-        print('nope')
-    soup = BeautifulSoup(response.content, 'html.parser')
+#update url in order to pull book links from each page within each category
+
     book_url = []
-    h3_tags = soup.find_all('h3')
-    #book_url = []
+    i = 1
+    while(True):
+        try:
+            if i == 1:
+                url = category_url
+               # print(url)
+            else:
+                url = category_url.replace('index.html', 'page-{}.html').format(i)
 
-    for article in h3_tags:
-        for link in article.find_all('a', href=True):
-            url = link['href']
-            links = 'http://books.toscrape.com/' + url
-            if links not in book_url:
-                book_url.append(
-                    f'https://books.toscrape.com/catalogue/{article.contents[0]['href'].replace("../../../", "")}')
+            r = requests.get(url)
+            if r.status_code == 200:
+                print('correct')
+            else:
+                print('incorrect')
+                if 'incorrect':
+                    break
+            soup = BeautifulSoup(r.content, 'html.parser')
+            print(url)
+# pull links for every book
+            h3_tags = soup.find_all('h3')
+            for article in h3_tags:
+                for link in article.find_all('a', href=True):
 
+                    url = link['href']
+                    links = 'http://books.toscrape.com/' + url
+
+                    if links not in book_url:
+                        book_url.append(
+                            f'https://books.toscrape.com/catalogue/{article.contents[0]['href'].replace("../../../", "")}')
+            i += 1
+        except:
+                break
+    print(book_url)
     return book_url
 
-def turn_page(url):
-    r = requests.get(url)
-    if r.status_code == 200:
-        print("continue")
-    else:
-        print('needs fixed')
-    soup = BeautifulSoup(r.content, 'html.parser')
 
-    next_page = soup.find('ul', class_='pager')
-    if next_page:
-        for page in next_page:
-            all_pages = page.find('li', class_='current').text
-            number = page.find(all_pages.strip()[10:])
 
-            counter = 2
-            while number > 1:
-                next_page_link = f'{url.replace('index.html', '')}page-{counter}.html'
-                url.append(next_page_link)
-                number -= 1
-                counter += 1
-    return turn_page(url)
 
 #pulling book information for individual books
 print("--------pending processing----------")
 def product_information(book_url):
-    # update so it's not an object,
     print('Ready to Read? -----obtaining book information, please hold-----')
     response = requests.get(book_url)
 
@@ -94,25 +85,19 @@ def product_information(book_url):
         titles = image['alt']
 
         # saving images for each book into folder "Cover Images
+        image_folder = Path('./Cover Images')
         if not os.path.exists('Cover Images'):
             os.makedirs('Cover Images')
 
 
-        # f = open('Cover Images/' + titles +'.jpg', 'wb')
-        r = requests.get(image_urls) #, stream=True)
+
+        r = requests.get(image_urls, stream=True)
+        upc = soup.find('table', class_ = "table table-striped").find_all ('td') [0].text.strip()
 
         if r.status_code == 200:
-          # fp = open('Cover Images/' + titles, 'wb')
-          # fp.write(r.content)
-          # fp.close()
-            r.raw.decode_content = True
+            with open(f'{'Cover Images/'} Cover-{upc}.jpg', 'wb') as f:
+                f.write(r.content)
 
-            with open('Cover Images/' + titles, 'wb') as f:
-                shutil.copyfileobj(r.raw, f)
-
-            print('Download Success: ', titles)
-        else:
-            print("image not retrieved")
 
         # obtaining information on book
 
@@ -136,8 +121,6 @@ def product_information(book_url):
         tax_data = soup.find('table', class_ = "table table-striped").find_all ('td') [4].text.strip()
         availability_data = soup.find('table', class_ = "table table-striped").find_all ('td') [5].text.strip()
         num_reviews_data = soup.find('table', class_ = "table table-striped").find_all ('td') [6].text.strip()
-           # print(upc_data, product_type_data, price_inc_tax_data,price_exc_tax_data,tax_data, availability_data, num_reviews_data)
-        images = soup.find_all('img', {'src': True})
 
         columns = ['Title', 'Description', 'Book Rating', 'Book Avaliability',
                     'UPC', 'Product Type', 'Book Price', 'PET', 'PIT', 'Tax',
@@ -158,45 +141,65 @@ def product_information(book_url):
                 'tax_name': tax_data,
                 'Availability': availability_data,
                 'Num_reviews_name': num_reviews_data,
-                'image url' : images,
+                'image url' : image_urls,
                 'category' : obtain_category
                     }
+        header = [
+            "Product Page URL",
+            "Image URL",
+            "Title",
+            "upc",
+            "price_inc_tax_name",
+            "Price_exc_tax_name",
+            "Available",
+            "Category",
+            "Reviews",
+            'Rating',
+            "Product Description"
+        ]
+        # create path for where books will go
+        path = f"BookFiles/"
+        Path(path).mkdir(parents=True, exist_ok=True)
 
 
+        downloadables = ({'Product Page URL': book_url,
+                              'upc': upc_data,
+                              'Title': titles,
+                              'price_inc_tax_name': price_inc_tax_data,
+                              'Price_exc_tax_name': price_exc_tax_data,
+                              'Available': book_availability,
+                              'Product Description': product_description,
+                              'Category': obtain_category,
+                              'Reviews': num_reviews_data,
+                              'Rating': book_rating,
+                              'Image URL': image_urls})
+
+        with open(f"BookFiles/{obtain_category}.csv", "a")as csv_file:
+                    write = csv.DictWriter(csv_file, fieldnames=header)
+                    if csv_file.tell() == 0:
+                        write.writeheader()
+
+
+            # create a loop for data to run through
+
+                    write.writerow({'Product Page URL': book_url,
+                            'upc': upc_data,
+                            'Title': titles,
+                            'price_inc_tax_name': price_inc_tax_data,
+                            'Price_exc_tax_name': price_exc_tax_data,
+                            'Available': book_availability,
+                            'Product Description': product_description,
+                            'Category': obtain_category,
+                            'Reviews': num_reviews_data,
+                            'Rating': book_rating,
+                            'Image URL': image_urls})
+                    print('-----CSV updated-----')
         return data
 
-#def obtain_image(book_url):
-    #response = requests.get(book_url)
 
-    #if response.status_code == 200:
-    #    soup = BeautifulSoup(response.content, 'html.parser')
-        # pulling image information
-   #     image = soup.find('img')
-   #     image_url = image['src']
-       # image_urls = image_url.replace("../../", "https://books.toscrape.com/")
-        # print(image_url)
-
-     #   titles = image['alt']
-    #    fp = open('Cover Images/' + titles, 'wb')
-   #     fp.write(response.content)
-  #      fp.close()
-#running links through product information to obtain info for csv
-def category_info(links):
-
-    print("linking it all together...")
-    info = []
-    for link in links:
-
-        book_info = product_information(link)
-        info.append(book_info)
-       # write_csv(info, book_info['category'])
-
-    return info
 
 #creating csv for each category
 def write_csv(data, category_name):
-# -----# need to figure out how to get only the csv for each individual category, not each csv adding onto previous info
-
 
     header = [
             "Product Page URL",
@@ -218,7 +221,7 @@ def write_csv(data, category_name):
 #open csv in the name of the category
     with open(f"BookFiles/{category_name}.csv", "w", newline="", encoding="utf-8") as file:
         write = csv.DictWriter(file, fieldnames=header)
-        write.writeheader()
+        write.writerow(header)
 
 #create a loop for data to run through
         for one_book in data:
@@ -235,12 +238,15 @@ def write_csv(data, category_name):
                                 'Reviews': one_book['Num_reviews_name'],
                                 'Rating': one_book['book_rating'],
                                 'Image URL': one_book['image url']})
-
-
 if __name__=='__main__':
-
+    info = []
     every_category = category_selection()
-    links = extract_book_links(every_category)
+    for category_link in every_category:
 
-    category_info(links)
+        links = extract_book_links(category_link)
+   
+        for link in links:
+            book_info = product_information(link)
+            info.append(book_info)
 
+   
